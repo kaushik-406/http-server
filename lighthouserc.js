08ce41924e5b3b1dee7c192dec3bf0cc09747d4f
kaushik.khara <kaushik.khara@teknopoint.in>
2022-06-16 20:10:36 +0530

    new files

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,13 @@
 module.exports = {
-    ci: {
-      collect: {
-        url: ['https://http-web-server.herokuapp.com'],
-        startServerCommand: 'rails server -e production',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+  ci: {
+    collect: {
+      url: ["https://http-web-server.herokuapp.com"],
+      startServerCommand: "rails server -e production",
     },
-  };
+    upload: {
+      target: "temporary-public-storage",
+      serverBaseUrl: "https://http-web-server.herokuapp.com",
+      token: "LIGHTHOUSE_APP_TOKEN", // could also use LHCI_TOKEN variable instead
+    },
+  },
+};
