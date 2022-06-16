dbd865c0d81237ea990fe2e753b6d385d59f7913
kaushik.khara <kaushik.khara@teknopoint.in>
2022-06-16 18:54:51 +0530

    new files

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,17 @@
 module.exports = {
-    ci: {
-      collect: {
-        startServerCommand: 'npm run start',
-        numberOfRuns: 3,
-        url: ['https://http-web-server.herokuapp.com']
-      },
-      upload: {
-        target: 'temporary-public-storage',
+  ci: {
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'csp-xss': 'warn',
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 1 }],
       },
     },
-  };
+  },
+};
